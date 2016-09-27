c2e02182805e0e9943a809c91ceea9bd5007424c
Floris van Breugel <florisvb@gmail.com>
2016-09-27 10:33:18 -0700

    first commit

---
+++ nodes/hardware_configurations.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from __future__ import division
+from optparse import OptionParser
+import roslib
+import rospy
+import rosparam
+import numpy as np
+from std_msgs.msg import Float32, Header, String
+from multi_alicat_control.msg import msg_bb9, msg_phidget_interface_ssr, msg_action_and_flowrate
+import time
+import imp
+
+class Rig(object):
+    def __init__(self,  action_and_flowrate_topic='/rig1', 
+                        bb9_topic='/alicat_bb9', 
+                        ssr_topic='/phidgets_interface_ssr', 
+                        rig_function_configuration='path_to_py_file'):
+        nodename = action_and_flowrate_topic.split('/')[1]
+        rospy.init_node(nodename)
+        
+        self.subscriber = rospy.Subscriber(action_and_flowrate_topic, msg_action_and_flowrate, self.control_hardware, queue_size=10)
+        self.bb9_publisher = rospy.Publisher(bb9_topic, msg_bb9, queue_size=10)
+        self.ssr_publisher = rospy.Publisher(ssr_topic, msg_phidget_interface_ssr, queue_size=10)
+        
+        self.rig_functions = imp.load_source('rig_functions', rig_function_configuration)
+        
+        rospy.spin()
+
+    def control_hardware(self, action_and_flowrate):
+        bb9_addresses, bb9_flowrates, ssr_ports, ssr_states = self.rig_functions.__getattribute__(action_and_flowrate.action)(action_and_flowrate.flowrate)
+
+        tnow = rospy.Time.now()
+        
+        bb9 = msg_bb9()
+        bb9.header.stamp = tnow
+        bb9.addresses = bb9_addresses
+        bb9.flowrates = bb9_flowrates
+        
+        ssr = msg_phidget_interface_ssr()
+        ssr.header.stamp = tnow
+        ssr.ports = ssr_ports
+        ssr.states = ssr_states
+
+        self.bb9_publisher.publish(bb9)
+        self.ssr_publisher.publish(ssr)
+        
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("--action_and_flowrate_topic", type="str", dest="action_and_flowrate_topic", default='/rig1',
+                        help="action_and_flowrate_topic")
+    parser.add_option("--bb9_topic", type="str", dest="bb9_topic", default="/alicat_bb9",
+                        help="topic to publish bb9 messages, for controlling alicats through the bb9 node (one of these topics per bb9 device)")
+    parser.add_option("--ssr_topic", type="str", dest="ssr_topic", default="/phidgets_interface_ssr",
+                        help="topic to publish ssr messages, for controlling through phidgets interface kit (one of these topics per interface kit)")        
+    parser.add_option("--rig_function_configuration", type="str", dest="rig_function_configuration", default='none',
+                        help="full path to configuration py file listing the rig functions")
+
+    (options, args) = parser.parse_args()
+    
+    
+    rig = Rig(options.action_and_flowrate_topic, options.bb9_topic, options.ssr_topic, options.rig_function_configuration)
---
+++ nodes/phidgets_interface_ssr.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+from __future__ import division
+
+# ROS imports
+import roslib
+import rospy
+import rosparam
+from multi_alicat_control.msg import msg_phidget_interface_ssr
+
+#Basic imports
+from ctypes import *
+import time
+import numpy as np
+import sys
+import random
+
+#Phidget specific imports
+from Phidgets.PhidgetException import PhidgetErrorCodes, PhidgetException
+from Phidgets.Events.Events import AttachEventArgs, DetachEventArgs, ErrorEventArgs, InputChangeEventArgs, OutputChangeEventArgs, SensorChangeEventArgs
+from Phidgets.Devices.InterfaceKit import InterfaceKit
+
+###############################################################################
+###############################################################################
+class PhidgetsDAQ:
+
+    def __init__(self):
+        rospy.init_node('phidgets_interface_ssr', anonymous=True)
+        
+    def initialize_digital_outputs(self, phidget):
+        '''
+        subscribes to /phidgets_interface_ssr topic, and connects received messages with setting the output on the phidget
+        '''
+        self.phidget = phidget
+        self.subscriber = rospy.Subscriber('/phidgets_interface_ssr', msg_phidgets_interface_ssr, self.set_digital_output)
+
+    def set_digital_output(self, digital_output):
+        for i in range(len(digital_output.ports)):
+            self.phidget.setOutputState(digital_output.ports[i], digital_output.states[i])    
+    
+    def run(self):
+        rospy.spin()
+        
+def connect_to_phidget(SensorChangedFunction, serial_number=None):
+    #Create an interfacekit object
+    try:
+        interfaceKit = InterfaceKit()
+    except RuntimeError as e:
+        print("Runtime Exception: %s" % e.details)
+        print("Exiting....")
+        exit(1)
+
+    #Information Display Function
+    def displayDeviceInfo():
+        print("|------------|----------------------------------|--------------|------------|")
+        print("|- Attached -|-              Type              -|- Serial No. -|-  Version -|")
+        print("|------------|----------------------------------|--------------|------------|")
+        print("|- %8s -|- %30s -|- %10d -|- %8d -|" % (interfaceKit.isAttached(), interfaceKit.getDeviceName(), interfaceKit.getSerialNum(), interfaceKit.getDeviceVersion()))
+        print("|------------|----------------------------------|--------------|------------|")
+        print("Number of Digital Inputs: %i" % (interfaceKit.getInputCount()))
+        print("Number of Digital Outputs: %i" % (interfaceKit.getOutputCount()))
+        print("Number of Sensor Inputs: %i" % (interfaceKit.getSensorCount()))
+
+    #Event Handler Callback Functions
+    def interfaceKitAttached(e):
+        attached = e.device
+        print("InterfaceKit %i Attached!" % (attached.getSerialNum()))
+
+    def interfaceKitDetached(e):
+        detached = e.device
+        print("InterfaceKit %i Detached!" % (detached.getSerialNum()))
+
+    def interfaceKitError(e):
+        try:
+            source = e.device
+            print("InterfaceKit %i: Phidget Error %i: %s" % (source.getSerialNum(), e.eCode, e.description))
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.details))
+
+    #Main Program Code
+    try:
+        interfaceKit.setOnAttachHandler(interfaceKitAttached)
+        interfaceKit.setOnDetachHandler(interfaceKitDetached)
+        interfaceKit.setOnErrorhandler(interfaceKitError)
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Exiting....")
+        exit(1)
+
+    print("Opening phidget object....")
+
+    try:
+        if serial_number is not None:
+            interfaceKit.openPhidget(serial_number)
+        else:
+            interfaceKit.openPhidget()
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Exiting....")
+        exit(1)
+
+    print("Waiting for attach....")
+
+    try:
+        interfaceKit.waitForAttach(10000)
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        try:
+            interfaceKit.closePhidget()
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.details))
+            print("Exiting....")
+            exit(1)
+        print("Exiting....")
+        exit(1)
+    else:
+        displayDeviceInfo()
+
+    phidget = interfaceKit
+    
+    return phidget
+
+if __name__ == '__main__':
+    
+    print 'connecting to phidget'
+    phidgets_daq = PhidgetsDAQ()
+    serial_number = rospy.get_param('/phidgets_interface_ssr/serial_number', None)
+    phidget = connect_to_phidget(serial_number)
+    
+    phidgets_daq.initialize_digital_outputs(phidget)
+    phidgets_daq.run()
+
